5fc9597ce4144f50d2475f2a1459e410a6e700d5
Stuart B. Wilkins <swilkins@bnl.gov>
2015-11-19 22:04:56 -0500

    [ENH] Fixed to use bitwise to strip bits

--- csxtools/image_corr.py
+++ csxtools/image_corr.py
@@ -12,13 +12,15 @@ def subtract_background(image, dark_image, gain=(1, 4, 8)):
     gain_mask_8 = (image & 0xC000) == 0xC000
     gain_mask_4 = (image & 0xC000) == 0x8000
     gain_mask_1 = (image & 0xC000) == 0x0000
-    image = (image << 2) >>2
+    image = image & 0x1FFF
     cor_image = image.astype(np.float16)
     cor_image -= gain_mask_8 * dark_image[2]
     cor_image -= gain_mask_4 * dark_image[1]
     cor_image -= gain_mask_1 * dark_image[0]
 
-    gain_image = (gain_mask_8 * gain[2]) + (gain_mask_4 * gain[1]) + (gain_mask_1 * gain[0])
+    gain_image = (gain_mask_8 * gain[2]) +
+                 (gain_mask_4 * gain[1]) +
+                 (gain_mask_1 * gain[0])
 
     return (cor_image * gain_image), gain_image
 
@@ -45,8 +47,8 @@ def correct_events(evs, data_key, dark_images, drop_raw=False):
             new_ev['data'].pop(data_key)
             new_ev['timestamps'].pop(data_key)
         yield new_ev
-        
-        
+
+
 def clean_images(header, pivot_key, timesource_key, dark_images=None, static_keys=None):
     if static_keys is None:
         static_keys = ['sx', 'sy', 'temp_a', 'temp_b', 'sz']
@@ -54,18 +56,18 @@ def clean_images(header, pivot_key, timesource_key, dark_images=None, static_key
     pv_desc = [d for d in header['descriptors'] if pivot_key in d['data_keys']][0]
     # sort out which descriptor has the key that we want to zip with to get time stamps
     ts_desc = [d for d in header['descriptors'] if timesource_key in d['data_keys']][0]
-    
+
     ts_events = get_events_generator(ts_desc)
     pv_events = get_events_generator(pv_desc)
     # fill_event works in place, sillyness to work around that
     pv_events = ((ev, fill_event(ev))[0] for ev in pv_events)
     pivoted_events = pivot_timeseries(pv_events, [pivot_key], static_keys)
-    
+
     if dark_images:
         pivoted_events = correct_events(pivoted_events, pivot_key, dark_images)
     merged_events = zip_events(pivoted_events, ts_events)
     out_ev = reset_time(merged_events, timesource_key)
-    yield from out_ev 
+    yield from out_ev
 
 def extract_darkfield(header, dark_key):
     cam_desc = [d for d in header['descriptors'] if dark_key in d['data_keys']][0]
