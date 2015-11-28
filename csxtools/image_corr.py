973721b2f73551898f036d2a2ad18473b8189054
Stuart B. Wilkins <swilkins@bnl.gov>
2015-11-27 22:44:01 -0500

    Fixed to use new background routines

--- csxtools/image_corr.py
+++ csxtools/image_corr.py
@@ -43,7 +43,7 @@ def correct_events(evs, data_key, dark_images, drop_raw=False):
                  'seq_no': ev['seq_no'],
                  'data': dict(ev['data']),
                  'timestamps': dict(ev['timestamps'])}
-        corr, gain_img = subtract_background(ev['data'][data_key], dark_images)
+        corr, gain_img, err_mask = subtract_background(ev['data'][data_key], dark_images)
         new_ev['data'][out_data_key] = corr
         new_ev['timestamps'][out_data_key] = ttime.time()
         if drop_raw:
