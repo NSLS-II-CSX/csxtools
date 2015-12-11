6d53d763283c572afc9097c1dcbdfccd51ad9c64
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-10 20:36:49 -0500

    [ENH] Added build structure for c modules

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from __future__ import (absolute_import, division, print_function)
+import setuptools
+from distutils.core import setup, Extension
+import numpy as np
+import versioneer
 
-import sys
-import warnings
-
-
-try:
-    from setuptools import setup
-except ImportError:
-    try:
-        from setuptools.core import setup
-    except ImportError:
-        from distutils.core import setup
-
-from distutils.core import setup
+fastccd = Extension('fastccd',
+                    sources=['src/fastccdmodule.c',
+                             'src/fastccd.c'])
 
 setup(
     name='csxtools',
-    version="0.0.0",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author='Brookhaven National Laboratory',
-    packages=['csxtools'],
+    packages=setuptools.find_packages(exclude=['src']),
+    ext_package='csxtools.ext',
+    include_dirs=[np.get_include()],
+    ext_modules=[fastccd],
+    install_requires=['numpy'],  # essential deps only
+    url='http://github.com/NSLS-II_CSX/csxtools',
+    keywords='Xray Analysis',
+    license='BSD'
 )
