62b22f7a72e2b53118280beadfd707f662813e99
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-12 12:19:42 -0500

    [ENH] Made dark images optional

--- csxtools/fastccd/image_corr.py
+++ csxtools/fastccd/image_corr.py
@@ -2,7 +2,13 @@ import numpy as np
 from ..ext import fastccd as fccd
 
 
-def correct_images(images, dark, flat=None, gain=(1, 4, 8)):
+def get_fastccd_images(light, dark = None):
+    """Retreive the
+
+    """
+
+
+def correct_images(images, dark=None, flat=None, gain=(1, 4, 8)):
     """Subtract backgrond and gain correct images
 
     This routine subtrtacts the backgrond and corrects the images
@@ -13,7 +19,7 @@ def correct_images(images, dark, flat=None, gain=(1, 4, 8)):
     in : array_like
         Input array of images to correct of shape (N, y, x)  where N is the
         number of images and x and y are the image size.
-    dark : array_like
+    dark : array_like, optional
         Input array of dark images. This should be of shape (3, y, x).
         dark[0] is the gain 8 (most sensitive setting) dark image with
         dark[2] being the gain 1 (least sensitive) dark image.
@@ -31,9 +37,10 @@ def correct_images(images, dark, flat=None, gain=(1, 4, 8)):
 
     """
 
+    if dark is None:
+        dark = np.zeros(images.shape[-2:], dtype=np.float32)
+        dark = np.array((dark, dark, dark))
     if flat is None:
-        flat = np.ones(dark.shape[1:], dtype=np.float32)
+        flat = np.ones(images.shape[-2:], dtype=np.float32)
 
     return fccd.correct_images(images, dark, flat, gain)
-
-
