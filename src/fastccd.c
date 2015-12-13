5dd15fc7ef31af156b9c35dd47b178ca2027e0b7
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-13 16:49:17 -0500

    [ENH] Un-nested loop

--- src/fastccd.c
+++ src/fastccd.c
@@ -52,10 +52,8 @@
 // Correct fast ccd images by looping over all images correcting for background
 int correct_fccd_images(uint16_t *in, data_t *out, data_t *bg, data_t *flat,
                         int ndims, index_t *dims, data_t* gain){
-  index_t nimages,i,k;
+  index_t nimages,k;
   int n;
-  uint16_t *inp;
-  data_t *bgp, *outp, *flatp;
 
   nimages = dims[0];
   for(n=1;n<(ndims-2);n++){
@@ -64,24 +62,19 @@ int correct_fccd_images(uint16_t *in, data_t *out, data_t *bg, data_t *flat,
 
   index_t imsize = dims[ndims-1] * dims[ndims-2];
 
-#pragma omp parallel for private(k,i,bgp,inp,outp,flatp) shared(in, out, bg, imsize, gain, flat)
-  for(k=0;k<nimages;k++){
+#pragma omp parallel for private(k) shared(in, out, bg, imsize, gain, flat) schedule(static,imsize)
+  for(k=0;k<nimages*imsize;k++){
     // Reset the background pointer each time
-    bgp = bg;
-    flatp = flat;
-    inp = in + (k * imsize);
-    outp = out + (k * imsize);
-    for(i=0;i<imsize;i++){
-      if((*inp & BAD_PIXEL) == BAD_PIXEL){
-        *outp = NAN;
-      } else if((*inp & GAIN_1) == GAIN_1){
-        *outp = *flatp * gain[2] * ((data_t)(*inp & PIXEL_MASK) - *(bgp + 2 * imsize));
-      } else if((*inp & GAIN_2) == GAIN_2){
-        *outp = *flatp * gain[1] * ((data_t)(*inp & PIXEL_MASK) - *(bgp + imsize));
-      } else {
-        *outp = *flatp * gain[0] * ((data_t)(*inp & PIXEL_MASK) - *bgp);
-      }
-      outp++; inp++; bgp++; flatp++;
+    data_t *bgp = bg + (k % imsize);
+    data_t *flatp = flat + (k % imsize);
+    if((in[k] & BAD_PIXEL) == BAD_PIXEL){
+      out[k] = NAN;
+    } else if((in[k] & GAIN_1) == GAIN_1){
+      out[k] = *flatp * gain[2] * ((data_t)(in[k] & PIXEL_MASK) - *(bgp + 2 * imsize));
+    } else if((in[k] & GAIN_2) == GAIN_2){
+      out[k] = *flatp * gain[1] * ((data_t)(in[k] & PIXEL_MASK) - *(bgp + imsize));
+    } else {
+      out[k] = *flatp * gain[0] * ((data_t)(in[k] & PIXEL_MASK) - *bgp);
     }
   }
 
