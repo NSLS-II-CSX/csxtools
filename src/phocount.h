919b663d57f4778f7b16b94b1b7308e7258c0e25
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-20 15:24:06 -0500

    [ENH] Added parameter

--- src/phocount.h
+++ src/phocount.h
@@ -42,7 +42,8 @@
 typedef long index_t;
 typedef float data_t;
 
-int count(data_t *in, data_t *out, data_t *stddev, int ndims, index_t *dims, data_t *thresh);
+int count(data_t *in, data_t *out, data_t *stddev, 
+          int ndims, index_t *dims, data_t *thresh, int sum_max);
 void sort(data_t *array, int n);
 
 #endif
