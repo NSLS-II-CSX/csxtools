aaaf87d34a0e44255a987b9498ec01d33cd7613a
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-21 04:26:33 -0500

    [FIX] Added __all__ for docs

--- csxtools/fastccd/__init__.py
+++ csxtools/fastccd/__init__.py
@@ -3,6 +3,9 @@ from .._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+__all__ = ['correct_images', 'phocount']
+
 # Now import useful functions
 
 from .images import correct_images
+from .phocount import phocount
