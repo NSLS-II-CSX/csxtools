d274b8c1ba355e96550b734e593a54833e413f89
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-21 04:26:33 -0500

    [FIX] Added __all__ for docs

--- csxtools/image/__init__.py
+++ csxtools/image/__init__.py
@@ -3,6 +3,8 @@ from .._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+__all__ = ['rotate90']
+
 # Now import useful functions
 
 from .transform import rotate90
