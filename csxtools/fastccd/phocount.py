bbf8bd82c554b71ffb19f7e16548993ba8dc4f50
Stuart B. Wilkins <swilkins@bnl.gov>
2015-12-22 05:37:14 -0500

    [DOC] Added documentation

--- csxtools/fastccd/phocount.py
+++ csxtools/fastccd/phocount.py
@@ -5,6 +5,27 @@ def phocount(data, thresh, nsum=3):
     """Do single photon counting on CCD image
 
     This routine does single photon counting by cluster analysis. The image
-    is searched for bright pixels within a threshold
+    is searched for bright pixels within a threshold and then the energy
+    deposited by each photon is calculated.
+
+    Parameters
+    ----------
+    data : array_like
+        Stack of CCD images. This array should be of shape (N, y, x) where
+        N is the number of images
+    thresh : tuple
+        Threshold to use for identifying photons. This should be a tuple of
+        (min, max)
+    nsum : int
+        The number of pixels to use to calculate the energy deposited by the
+        photon. This should be 0 < nsum <= 9.
+
+    Returns
+    -------
+    tuple
+        Two arrays are returned. The first is an array of size (N, y, x)
+        where the elements are the integrated energy calculated for each
+        photon hit. The second array is the standard deviation for the
+        integrated intensity on each photon hit.
     """
     return ph(data, thresh, nsum)
