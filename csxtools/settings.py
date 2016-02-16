ae791f6974632a52c4e0288e0e60ae1e1c435475
Stuart B. Wilkins <swilkins@bnl.gov>
2016-02-16 09:07:19 -0500

    [ENH] Added config for recip angles

--- csxtools/settings.py
+++ csxtools/settings.py
@@ -1,3 +1,13 @@
+# Define some config for the CSX beamline.
+# First, lets define the FastCCD config.
 detectors = {}
-detectors['fccd'] = 'fccd_image'
-diff_angles = ['delta', 'theta', 'gamma', None, None, None]
+detectors['fccd'] = {'tag': 'fccd_image',
+                     'pixel_size': (0.03, 0.03),  # mm
+                     'calibrated_center': (960.0/2, 960.0/2),  # pixels
+                     'dist_sample': (355.0)}  # mm
+
+# Now lets define the reciprocal space angles which are derived
+# from the motor positions.
+# These are (delta, theta, gamma, chi, phi, mu)
+
+diff_angles = ('delta', 'theta', 'gamma', -90.0, 0.0, 0.0)
