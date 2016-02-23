2f6362d84c4cba9c842b0b50ab4d76a735a7fd65
Stuart B. Wilkins <swilkins@bnl.gov>
2016-02-23 07:45:41 -0500

    Fixed PEP8

--- tests/test_image.py
+++ tests/test_image.py
@@ -50,7 +50,7 @@ def test_stacksum():
     x[10] = np.nan
     x[23] = np.nan
     x[40] = np.nan
-    m,n = stacksum(x)
+    m, n = stacksum(x)
     assert_array_equal(m, np.ones((100, 100), dtype=np.float32) *
                        2 * (1000 - 3))
     assert_array_equal(n, np.ones((100, 100), dtype=np.float32) * (1000 - 3))
