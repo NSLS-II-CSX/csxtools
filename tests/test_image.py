754d0b798b253b39d6214c2c5c3ec7b63db153ff
vivekthampy <vivekthampy@hotmail.com>
2016-03-01 14:41:54 -0500

    corrected bug

--- tests/test_image.py
+++ tests/test_image.py
@@ -88,6 +88,7 @@ def test_images_mean():
         (ii*100, 20, 20)))
     x = np.array(x)
     m = images_mean(x)
+    assert_array_almost_equal(m, np.array([np.mean(x1) for x1 in x]), 3)
 
 
 def test_images_sum():
@@ -97,4 +98,4 @@ def test_images_sum():
         (ii*100, 20, 20)))
     x = np.array(x)
     m = images_sum(x)
-    assert_array_almost_equal(m, np.array([np.sum(np.mean(x1, axis=0) for x1 in x]), 3)
+    assert_array_almost_equal(m, np.array([np.sum(np.mean(x1, axis=0)) for x1 in x]), 3)
