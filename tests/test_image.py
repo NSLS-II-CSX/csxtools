e020b10c5c1e333d94c772cd189c717188a23152
Stuart B. Wilkins <swilkins@bnl.gov>
2016-05-07 17:55:08 -0400

    TST: Changed precision

--- tests/test_image.py
+++ tests/test_image.py
@@ -52,7 +52,8 @@ def test_stacksum():
     x[23] = np.nan
     x[40] = np.nan
     m, n = stacksum(x)
-    assert_array_almost_equal(m, np.ones((100, 100), dtype=np.float32) * 2000)
+    assert_array_almost_equal(m, np.ones((100, 100), dtype=np.float32) * 2000,
+                              decimal=3)
     assert_array_equal(n, np.ones((100, 100), dtype=np.float32) * (1000 - 3))
 
 
