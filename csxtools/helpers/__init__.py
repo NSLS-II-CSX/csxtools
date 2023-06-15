ea31baa9f319f0a6253c36dfae05af4e16860b21
Andi Barbour <ambarb@users.noreply.github.com>
2023-06-15 19:14:33 -0400

    fixed improts for get_fccd... to get_fastccd ...

--- csxtools/helpers/__init__.py
+++ csxtools/helpers/__init__.py
@@ -1,7 +1,7 @@
-from .fastccd import (get_dark_near, get_dark_near_all, get_fccd_roi, get_fccd_exp, get_fastccd_images_sized, convert_photons)
+from .fastccd import (get_dark_near, get_dark_near_all, get_fastccd_roi, get_fastccd_exp, get_fastccd_images_sized, convert_photons)
 from .overscan import (get_os_correction_images, get_os_dropped_images)
 
-__all__ = ['get_dark_near', 'get_dark_near_all', 'get_fccd_roi', 'get_fccd_exp', 'get_fastccd_images_sized', 'convert_photons', 'get_os_correction_images', 'get_os_dropped_images']
+__all__ = ['get_dark_near', 'get_dark_near_all', 'get_fastccd_roi', 'get_fastccd_exp', 'get_fastccd_images_sized', 'convert_photons', 'get_os_correction_images', 'get_os_dropped_images']
 
 # set version string using versioneer
 from .._version import get_versions
