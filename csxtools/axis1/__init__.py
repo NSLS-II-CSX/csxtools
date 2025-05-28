45ad128ece0913af4066a652aa651b0909acabe4
nisar <nisar.n.k@gmail.com>
2025-05-28 11:54:08 -0400

    The photon count is removed for axis detector. A bug originated from this (csxtools/axis1/__init__.py) is fixed

--- csxtools/axis1/__init__.py
+++ csxtools/axis1/__init__.py
@@ -1,8 +1,6 @@
-# from .images import correct_images
 from .images import correct_images_axis
-from .phocount import photon_count
 
-__all__ = ["correct_images_axis", "photon_count"]
+__all__ = ["correct_images_axis"]
 
 # set version string using versioneer
 from .._version import get_versions
