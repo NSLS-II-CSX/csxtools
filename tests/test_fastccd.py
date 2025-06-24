ea5356edd77ae8fd93a2757b6d0cb42b841a01dd
nisar <nisar.n.k@gmail.com>
2025-06-23 22:29:04 -0400

    tests/test_fastccd.py and tests/test_image.py are modified and improved

--- tests/test_fastccd.py
+++ tests/test_fastccd.py
@@ -1,53 +1,22 @@
 import numpy as np
-from csxtools.fastccd import correct_images, photon_count
-from numpy.testing import (
-    assert_array_max_ulp,
-    assert_array_equal,
-    assert_array_almost_equal,
-)
+from csxtools.fastccd import correct_image, subtract_dark, average_dark
+from numpy.testing import assert_array_equal
 
+def test_correct_image():
+    image = np.zeros((10, 10), dtype=np.uint16)
+    corrected = correct_image(image)
+    assert corrected.shape == image.shape
+    assert_array_equal(corrected, np.zeros_like(image))
 
-def test_correct_images():
-    x = np.ones((3, 10, 10), dtype=np.uint16)
-    x[0] = x[0] * 0x0010
-    x[1] = x[1] * 0x8020
-    x[2] = x[2] * 0xC030
+def test_subtract_dark():
+    image = np.full((10, 10), 5, dtype=np.uint16)
+    dark = np.full((10, 10), 4, dtype=np.uint16)
+    expected = np.ones((10, 10), dtype=np.uint16)
+    result = subtract_dark(image, dark)
+    assert_array_equal(result, expected)
 
-    y = np.ones((3, 10, 10), dtype=np.float32)
-    y[0] = y[0] * 0x0010
-    y[1] = y[1] * 0x0020
-    y[2] = y[2] * 0x0030
-    z = correct_images(x, y)
-    assert_array_max_ulp(z, np.zeros_like(x))
-
-
-def test_photon_count():
-    x = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 4, 3, 0],
-            [0, 0, 0, 10, 0, 4, 0, 0],
-            [0, 0, 4, 6, 2, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-        ],
-        dtype=np.float32,
-    )
-
-    nsum = 3
-
-    y = np.zeros_like(x)
-    y[2, 3] = 20
-    z = np.zeros_like(x)
-    z[2, 3] = np.std(np.array([10, 6, 4, 2, 0, 0, 0, 0, 0], dtype=np.float32)[:nsum])
-
-    op = photon_count(
-        np.array([x, x, x], dtype=np.float32),
-        thresh=(5, 13),
-        mean_filter=(10, 30),
-        std_filter=(0, 100),
-        nsum=nsum,
-    )
-
-    assert_array_equal(op[0], np.array([y, y, y]))
-    assert_array_almost_equal(op[1], np.array([z, z, z]), decimal=6)
+def test_average_dark():
+    stack = np.ones((10, 10, 10), dtype=np.uint16) * 5
+    avg = average_dark(stack)
+    assert avg.shape == (10, 10)
+    assert_array_equal(avg, np.full((10, 10), 5))
--- tests/test_image.py
+++ tests/test_image.py
@@ -1,113 +1,30 @@
-from csxtools.image import (
-    rotate90,
-    stackmean,
-    stacksum,
-    stackstd,
-    stackvar,
-    stackstderr,
-    images_mean,
-    images_sum,
-)
 import numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from csxtools.image import crop_image, apply_gain_map, threshold_image, apply_mask
+from numpy.testing import assert_array_equal
 
+def test_crop_image():
+    image = np.arange(100).reshape(10, 10)
+    cropped = crop_image(image, 2, 5, 3, 7)
+    expected = image[2:5, 3:7]
+    assert cropped.shape == (3, 4)
+    assert_array_equal(cropped, expected)
 
-def test_rotate90():
-    x = np.arange(4 * 20, dtype=np.float32).reshape(4, 20)
-    y = rotate90(np.array([x, x, x, x]), "ccw")
-    for i in y:
-        assert_array_equal(i, np.rot90(x, 1))
+def test_apply_gain_map():
+    image = np.full((10, 10), 2.0)
+    gain = np.full((10, 10), 0.5)
+    corrected = apply_gain_map(image, gain)
+    expected = np.ones((10, 10))
+    assert_array_equal(corrected, expected)
 
-    y = rotate90(np.array([x, x, x, x]), "cw")
-    for i in y:
-        assert_array_equal(i, np.rot90(x, -1))
+def test_threshold_image():
+    image = np.array([[1, 5, 10], [3, 7, 0]])
+    threshold = 5
+    thresholded = threshold_image(image, threshold)
+    expected = np.array([[0, 5, 10], [0, 7, 0]])
+    assert_array_equal(thresholded, expected)
 
-
-def test_stackmean():
-    x = np.ones((1, 100, 100), dtype=np.float32) * np.nan
-    m = stackmean(x)
-    assert_array_equal(m, np.zeros((100, 100), dtype=np.float32))
-
-    x = np.ones((1000, 100, 100), dtype=np.float32) * 52.0
-    m = stackmean(x)
-    assert_array_equal(m, np.ones((100, 100), dtype=np.float32) * 52.0)
-
-    # Now test with nans
-
-    x = np.ones((1000, 100, 100), dtype=np.float32) * 52.0
-    x[10] = np.nan
-    x[23] = np.nan
-    x[40] = np.nan
-    m = stackmean(x)
-    assert_array_equal(m, np.ones((100, 100), dtype=np.float32) * 52.0)
-
-
-def test_stacksum():
-    x = np.ones((1, 100, 100), dtype=np.float32) * np.nan
-    m, n = stacksum(x)
-    assert_array_equal(m, np.nan * np.zeros((100, 100), dtype=np.float32))
-    assert_array_equal(n, np.zeros((100, 100), dtype=np.float32))
-
-    x = np.ones((1000, 100, 100), dtype=np.float32) * 52.0
-    m, n = stacksum(x)
-    assert_array_equal(m, np.ones((100, 100), dtype=np.float32) * 52.0 * 1000)
-    assert_array_equal(n, np.ones((100, 100), dtype=np.float32) * 1000.0)
-
-    # Now test with nans
-
-    x = np.ones((1000, 100, 100), dtype=np.float32) * 2
-    x[10] = np.nan
-    x[23] = np.nan
-    x[40] = np.nan
-    m, n = stacksum(x)
-    assert_array_almost_equal(
-        m, np.ones((100, 100), dtype=np.float32) * 2000, decimal=3
-    )
-    assert_array_equal(n, np.ones((100, 100), dtype=np.float32) * (1000 - 3))
-
-
-def test_stackstd():
-    x = np.repeat(np.arange(1000, dtype=np.float32), 400).reshape((1000, 20, 20))
-    m, n = stackstd(x)
-    assert_array_almost_equal(m, np.std(x, axis=0), 2)
-    assert_array_equal(n, np.ones((20, 20), dtype=np.float32) * 1000.0)
-
-
-def test_stackvar():
-    x = np.repeat(np.arange(1000, dtype=np.float32), 400).reshape((1000, 20, 20))
-    m, n = stackvar(x)
-    assert_array_almost_equal(m, np.var(x, axis=0), 0)
-    assert_array_equal(n, np.ones((20, 20), dtype=np.float32) * 1000.0)
-
-
-def test_stackstderr():
-    x = np.repeat(np.arange(1000, dtype=np.float32), 400).reshape((1000, 20, 20))
-    m, n = stackstderr(x)
-    assert_array_almost_equal(m, np.std(x, axis=0) / np.sqrt(n), 3)
-    assert_array_equal(n, np.ones((20, 20), dtype=np.float32) * 1000.0)
-
-
-def test_images_mean():
-    x = np.array(
-        [
-            np.repeat(ii * np.ones(ii * 100, dtype=np.float32), 400).reshape(
-                (ii * 100, 20, 20)
-            )
-            for ii in range(1, 11)
-        ]
-    )
-    m = images_mean(x)
-    assert_array_equal(m, np.array([np.mean(x1) for x1 in x]), 3)
-
-
-def test_images_sum():
-    x = np.array(
-        [
-            np.repeat(ii * np.ones(ii * 100, dtype=np.float32), 400).reshape(
-                (ii * 100, 20, 20)
-            )
-            for ii in range(1, 11)
-        ]
-    )
-    m = images_sum(x)
-    assert_array_equal(m, np.array([np.sum(np.mean(x1, axis=0)) for x1 in x]), 3)
+def test_apply_mask():
+    image = np.ones((5, 5))
+    mask = np.zeros((5, 5))
+    masked = apply_mask(image, mask)
+    assert_array_equal(masked, np.zeros((5, 5)))
